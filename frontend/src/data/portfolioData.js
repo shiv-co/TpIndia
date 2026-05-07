30aa270d6237240ad554f282ddeb2cfcc5cc013f
Shivam Singh <fakegod436@gmail.com>
2026-05-07 15:31:30 +0530

    fix: correct casing in import path for MahaKhumb

--- frontend/src/data/portfolioData.js
+++ frontend/src/data/portfolioData.js
@@ -33,7 +33,7 @@ import The_mercy_man from "../assets/images/The_mercy_man.webp";
 import saajhi_short_film from "../assets/images/saajhi_short_film.webp";
 import bosa_short_Film from "../assets/images/bosa_short_Film.webp";
 // Government Documentaries
-import MahaKhumb from "../assets/images/mahakhumb.webp";
+import MahaKhumb from "../assets/images/MahaKhumb.webp";
 import Baisc_shiksha_vibhaag from "../assets/images/Baisc_shiksha_vibhaag.webp";
 import Ayodhya_documentary from "../assets/images/Ayodhya_doc.webp";
 import Mission_Rojgar from "../assets/images/Mission_rojgar.webp";
